da14c49cea0e236a9776379b7f434893ffa1e0bd
tparry2 <tparry@mymail.mines.edu>
2020-02-22 11:23:49 -0700

    second

---
+++ app/src/main/java/tparry/csci448/com/pixe/MainActivity.kt
@@ -0,0 +1,12 @@
+package tparry.csci448.com.pixe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
